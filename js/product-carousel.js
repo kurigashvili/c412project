c75bea2e87d681052710e5c31c47707cf291214d
unknown <kurigashvili@gmail.com>
2015-07-14 21:54:00 -0500

    added carousel with clients to product page

---
+++ js/product-carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $("#product-carousel").owlCarousel({
+
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+      items : 5,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+
+  });
+
+});
